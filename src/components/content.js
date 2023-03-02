a137f90cfdc73d85c6019a0e885a1dcfabb464a9
Kotchaporn62001 <126721625+Kotchaporn62001@users.noreply.github.com>
2023-03-02 19:36:13 +0000

    sto

--- src/components/content.js
+++ src/components/content.js
@@ -1,12 +1,18 @@
-import React from "react";
-import './content.css'
-import { FiBookOpen } from "react-icons/fi";
+import React, { useState } from 'react';
+import './content.css';
+import { FiBookOpen, FiBook } from "react-icons/fi";
 
 let ficdata= {
-    fic1 :"หมาป่ากับลูกแกะ",
-    story1 :"เช้าตรู่วันหนึ่ง ลูกแกะหลงฝูงยืนดื่มน้ำอยู่บริเวณริมฝั่งลำธารในป่า ในเช้าวันเดียวกันนั้นเอง หมาป่าหิวโซก็ย่างกายลงมาจากต้นน้ำเพื่อล่าเหยื่อ ไม่ช้าไม่นานมันก็ ..."
+    fic1 :"เด็กเลี้ยงแกะ",
+    story1 :"ณ หมู่บ้านชายป่า มีเด็กเลี้ยงแกะคนหนึ่งชอบพูดโกหกเป็นประจำ วันหนึ่งเกิดนึกสนุกอยากแกล้งชาวบ้านจึงร้องตะโกนว่า 'ช่วยด้วย ๆ หมาป่ามันจะมากินแกะแล้ว' ชาวบ้านต่างพากันมาช่วย พอเด็กเลี้ยงแกะเห็นชาวบ้านวิ่งหน้าตาตื่นก็หัวเราะด้วยความชอบใจ แล้วชอบเล่นสนุปแบบนี้อีกหลายครั้ง ชาวบ้านก็พากันวิ่งหน้าตาตื่นมาช่วยเขาทุกครั้ง และพบว่าพวกเขาถูกหลอกอีกเช่นเคย จนวันหนึ่งหมาป่าก็มาจริง ๆ คราวนี้เด็กเลี้ยงแกะตะโกนให้คนมาช่วยสุดเสียง 'ช่วยด้วย ๆ หมาป่ามันจะมากินแกะแล้ว' แต่ครั้งนี้กลับไม่มีชาวบ้านออกมาช่วยเด็กเลี้ยงแกะอีกแล้ว เพราะคิดว่าเขาคงจะโกหกอีก สุดท้ายเจ้าหมาป่าจึงกินแกะของเด็กเลี้ยงแกะไปทีละตัว ๆ จนหมด",
+    motto1 :"นิทานเรื่องนี้สอนให้รู้ว่า : คนที่ชอบโกหก แม้พูดควางจริงก็ไม่มีใครเชื่อ"
 }
-function content() {
+function Content() { 
+
+        const [click, setClick ] = useState(false);
+        const handleClick = () => setClick(!click);
+        console.log(click);
+
     return(
         <div className="content">
             <div className="container">
@@ -14,10 +20,23 @@ function content() {
                     <h1>รวมนิทานก่อนนอน</h1>
                 </div>
                 <section className="content_fic">
-                    <div className="fic_area">
-                        <img scr="" alt=""></img>
-                        <h2>{ficdata.fic1}<FiBookOpen/></h2>
-                        <p>{ficdata.story1}</p>
+                    <div className= "fic_area" >
+                        <div className="fic_data">
+                            <img src='https://cdn.pixabay.com/photo/2016/08/10/06/22/cartoon-1582537_960_720.jpg' alt="" width="100" height="100"/>
+                            <h2>{ficdata.fic1}</h2>
+                            <div className='logo_book' onClick={handleClick }>
+                                {click ?(
+                                <FiBookOpen/>
+                                ):(
+                                    <FiBook/>
+                                )}
+                            </div>
+                            <div className= {click? "text active" : "text"}>
+                                <p>{ficdata.story1}<br/> {ficdata.motto1}</p>
+                            </div>
+                            
+                        </div>
+                        
                     </div>
                 </section>
             </div>
@@ -25,4 +44,4 @@ function content() {
 
     )
 }
- export default content;
+ export default Content;
